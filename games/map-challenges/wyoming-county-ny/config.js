52386f2bcd2334e8e9136d00c05d41b211d51e09
purplesteve6 <purplesteve6@gmail.com>
2026-02-13 14:36:24 -0500

    Update config.js

--- games/map-challenges/wyoming-county-ny/config.js
+++ games/map-challenges/wyoming-county-ny/config.js
@@ -75,7 +75,7 @@ window.MAP_CHALLENGE_CONFIG = {
 
     overlayKicker: "MAP CHALLENGE",
     overlayTitle: "WYOMING COUNTY",
-    beginMessage: "Click each of the 16 towns in Wyoming County, New York as fast as you can!",
+    beginMessage: "How fast can you identify the 16 towns of Wyoming County, NY?",
 
     logoSrc: "/assets/images/logo/MSHistory_Logo_Small.png"
   }
