7716e352f9d6292e9d3f108b9b5e386ea8d09c18
purplesteve6 <purplesteve6@gmail.com>
2026-02-19 16:00:11 -0500

    Add Battles of the American Revolution

---
+++ 7/BattlesOfTheAmericanRevolution/config.js
@@ -0,0 +1,9 @@
+window.TOPIC_CONFIG = {
+  gradeKicker: "Grade 7",
+  title: "Battles of the American Revolution",
+  subtitle: "Middle School History",
+  homeLabel: "Battles of the American Revolution Home",
+  homeHref: "/7/BattlesOfTheAmericanRevolution/",
+  badgeIcon: "/assets/images/icons/7_icon.png",
+  mainLogo: "/assets/images/logo/MSHistory_Logo_Basic_Web.png"
+};
