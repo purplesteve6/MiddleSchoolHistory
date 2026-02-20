7d9e901f73f887e9e3381da8140061362ce19d39
purplesteve6 <purplesteve6@gmail.com>
2026-02-20 11:11:15 -0500

    Update timeline-core.js

--- timeline/timeline-core.js
+++ timeline/timeline-core.js
@@ -791,14 +791,15 @@ if (interval === "decade"){
     : `${d0}s`;
 }
 
-    }
-
-
-// Century label (100s, 200s, -300s, etc.)
+// Century label
 const c0 = Math.floor(histY/100) * 100;
-return `${c0}s`;
+return (c0 < 0)
+  ? `${Math.abs(c0)}s BCE`
+  : `${c0}s`;
 
-    }
+
+
+
   }
 
   if (document.readyState === "loading") {
