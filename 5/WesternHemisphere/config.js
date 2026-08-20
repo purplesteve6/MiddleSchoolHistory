9c6fee2e20ac4c39f8e417cefad007d38340c294
purplesteve6 <purplesteve6@gmail.com>
2026-08-20 13:05:37 -0400

    Add initial Grade 5, Western Hemisphere Files

---
+++ 5/WesternHemisphere/config.js
@@ -0,0 +1,9 @@
+window.TOPIC_CONFIG = {
+  gradeKicker: "Grade 5",
+  title: "The Western Hemisphere: Four Regions",
+  subtitle: "Middle School History",
+  homeLabel: "Western Hemisphere Home",
+  homeHref: "/5/WesternHemisphere/",
+  badgeIcon: "/assets/images/icons/5_icon.png",
+  mainLogo: "/assets/images/logo/MSHistory_Logo_Basic_Web.png"
+};
