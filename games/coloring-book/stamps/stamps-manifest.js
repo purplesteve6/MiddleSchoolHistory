02100dde9c889d4ba1cb4cacd4822ea6761f0a7d
purplesteve6 <purplesteve6@gmail.com>
2026-08-31 12:59:06 -0400

    Stamp Naming Fix

--- games/coloring-book/stamps/stamps-manifest.js
+++ games/coloring-book/stamps/stamps-manifest.js
@@ -1,11 +1,11 @@
 window.COLORING_BOOK_STAMPS = [
-  { id: 'speech-bubble-1', label: 'Speech Bubble 1', file: '/games/coloring-book/stamps/_speech_bubble_1.svg' },
-  { id: 'speech-bubble-2', label: 'Speech Bubble 2', file: '/games/coloring-book/stamps/_speech_bubble_2.svg' },
-  { id: 'speech-bubble-3', label: 'Speech Bubble 3', file: '/games/coloring-book/stamps/_speech_bubble_3.svg' },
-  { id: 'speech-bubble-4', label: 'Speech Bubble 4', file: '/games/coloring-book/stamps/_speech_bubble_4.svg' },
-  { id: 'speech-bubble-5', label: 'Thought Bubble 1', file: '/games/coloring-book/stamps/_thought_bubble_1.svg' },
-  { id: 'speech-bubble-6', label: 'Thought Bubble 1', file: '/games/coloring-book/stamps/_thought_bubble_2.svg' },
-  { id: 'speech-bubble-7', label: 'Burst 1', file: '/games/coloring-book/stamps/_burst_1.svg' },
-  { id: 'speech-bubble-8', label: 'Burst 2', file: '/games/coloring-book/stamps/_burst_2.svg' }
+  { id: 'speech-bubble-1', label: 'Speech Bubble 1', file: '/games/coloring-book/stamps/speech_bubble_1.svg' },
+  { id: 'speech-bubble-2', label: 'Speech Bubble 2', file: '/games/coloring-book/stamps/speech_bubble_2.svg' },
+  { id: 'speech-bubble-3', label: 'Speech Bubble 3', file: '/games/coloring-book/stamps/speech_bubble_3.svg' },
+  { id: 'speech-bubble-4', label: 'Speech Bubble 4', file: '/games/coloring-book/stamps/speech_bubble_4.svg' },
+  { id: 'speech-bubble-5', label: 'Thought Bubble 1', file: '/games/coloring-book/stamps/thought_bubble_1.svg' },
+  { id: 'speech-bubble-6', label: 'Thought Bubble 1', file: '/games/coloring-book/stamps/thought_bubble_2.svg' },
+  { id: 'speech-bubble-7', label: 'Burst 1', file: '/games/coloring-book/stamps/burst_1.svg' },
+  { id: 'speech-bubble-8', label: 'Burst 2', file: '/games/coloring-book/stamps/burst_2.svg' }
 
 ];
