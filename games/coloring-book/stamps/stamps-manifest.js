45c06ae9ba2ade0ed982509b7866065f52e9c3bf
purplesteve6 <purplesteve6@gmail.com>
2026-09-01 13:42:27 -0400

    Added Caption Stamp

--- games/coloring-book/stamps/stamps-manifest.js
+++ games/coloring-book/stamps/stamps-manifest.js
@@ -1,4 +1,5 @@
 window.COLORING_BOOK_STAMPS = [
+  { id: 'caption-box-1', label: 'Caption Box 1', file: '/games/coloring-book/stamps/caption_box_1.svg' },
   { id: 'speech-bubble-1', label: 'Speech Bubble 1', file: '/games/coloring-book/stamps/speech_bubble_1.svg' },
   { id: 'speech-bubble-2', label: 'Speech Bubble 2', file: '/games/coloring-book/stamps/speech_bubble_2.svg' },
   { id: 'speech-bubble-3', label: 'Speech Bubble 3', file: '/games/coloring-book/stamps/speech_bubble_3.svg' },
